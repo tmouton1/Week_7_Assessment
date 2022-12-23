da0a771a0353714e401385bde4cc9bd8bc107843
tmouton1 <tamia.mouton1@gmail.com>
2022-12-23 13:49:02 -0500

    added code.js to folder

---
+++ Code.js
@@ -0,0 +1,46 @@
+
+let array = [1,2,4,-4]
+
+let value = false;
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (i !== j) 
+
+    {if (array[i] + array[j] === 0) {
+        value = true;
+      } 
+    }
+  }
+}
+
+console.log(value)
+
+// runtime = O(n)
+
+function hasUniqueChars(str) {
+
+let unique = ""
+let count = 0;
+
+for(let i = 0; i < str.length; i++){
+for(let j = i; j<str.length; j++){
+  if (str[i] == str[j]) {
+    
+   
+  }
+  
+  } return unique;
+}
+
+}
+console.log(hasUniqueChars("Moonday"))
+
+// runtime 0(n^2)
+
+function isPanagram(str) {
+
+  let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+ 
+}
+
